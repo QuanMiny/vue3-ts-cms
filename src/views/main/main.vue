dc90305f63c0daae78741a0f0407cec4198bdee7
Miny <944307891@qq.com>
2022-01-10 22:58:40 +0800

    build(project): init project and add element-plus

---
+++ src/views/main/main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>Main</div>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <el-input v-model="input" placeholder="Please input" />
+  <el-tag type="success">标签二</el-tag>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const input = ref('')
+
+    return {
+      input
+    }
+  }
+})
+</script>
+
+<style scoped></style>
