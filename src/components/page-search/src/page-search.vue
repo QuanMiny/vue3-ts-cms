dd146c23c91eca09c37c1b207418924d0615859c
Miny <944307891@qq.com>
2022-03-28 21:14:05 +0800

    feat(page-search): 封装头部检索表单控件

---
+++ src/components/page-search/src/page-search.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="page-search">
+    <ym-form v-bind="searchFormConfig" v-model="formData">
+      <template #header>
+        <h3>检索</h3>
+      </template>
+      <template #footer>
+        <div class="handle-btns">
+          <el-button :icon="RefreshIcon">重置</el-button>
+          <el-button type="primary" :icon="SearchIcon">搜索</el-button>
+        </div>
+      </template>
+    </ym-form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  Refresh as RefreshIcon,
+  Search as SearchIcon
+} from '@element-plus/icons-vue'
+
+import YmForm from '@/base-ui/form'
+import { ref, defineProps } from 'vue'
+
+defineProps({
+  searchFormConfig: {
+    type: Object,
+    require: true
+  }
+})
+
+const formData = ref({
+  id: '',
+  name: '',
+  password: '',
+  sport: '',
+  createTime: ''
+})
+</script>
+
+<style lang="less" scoped>
+.handle-btns {
+  text-align: right;
+  padding: 0 50px 20px;
+}
+</style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,41 +1,12 @@
 <template>
   <div class="user">
-    <ym-form v-bind="searchFormConfig" v-model="formData">
-      <template #header>
-        <h3>检索</h3>
-      </template>
-      <template #footer>
-        <div class="handle-btns">
-          <el-button :icon="RefreshIcon">重置</el-button>
-          <el-button type="primary" :icon="SearchIcon">搜索</el-button>
-        </div>
-      </template>
-    </ym-form>
+    <page-search :search-form-config="searchFormConfig" />
   </div>
 </template>
 
 <script lang="ts" setup>
-import {
-  Refresh as RefreshIcon,
-  Search as SearchIcon
-} from '@element-plus/icons-vue'
-
-import YmForm from '@/base-ui/form'
+import PageSearch from '@/components/page-search'
 import { searchFormConfig } from './config/search.config'
-import { ref } from 'vue'
-
-const formData = ref({
-  id: '',
-  name: '',
-  password: '',
-  sport: '',
-  createTime: ''
-})
 </script>
 
-<style scoped>
-.handle-btns {
-  text-align: right;
-  padding: 0 50px 20px;
-}
-</style>
+<style scoped></style>
